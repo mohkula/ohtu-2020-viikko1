13ca9ffb071b964f47577faf6466a51803fe3071
Matias Himanen <matias.himanen@helsinki.fi>
2020-10-28 20:22:33 +0200

    varastotestejä

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -20,6 +20,14 @@ public class VarastoTest {
         varasto = new Varasto(10);
     }
 
+
+    @Test
+    public void negatiivinenTilavuus(){
+        varasto = new Varasto(-10);
+        assertEquals(0.0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
         assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
@@ -39,12 +47,36 @@ public class VarastoTest {
     }
 
     @Test
+    public void toStringToimii(){
+        varasto.lisaaVarastoon(3);
+
+        assertEquals(varasto.toString(),"saldo = " + 3.0 + ", vielä tilaa " + 7.0);
+    }
+
+    @Test
     public void lisaysLisaaPienentaaVapaataTilaa() {
         varasto.lisaaVarastoon(8);
 
         // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         assertEquals(2, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+
+    @Test
+    public void negatiivinenLisaysToimii() {
+        varasto.lisaaVarastoon(-8);
+
+        // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        assertEquals(0.0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    @Test
+    public void liikaLisaysToimii() {
+        varasto.lisaaVarastoon(20);
+
+        // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        assertEquals(10.0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+
 
     @Test
     public void ottaminenPalauttaaOikeanMaaran() {
@@ -64,5 +96,67 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+
+     @Test
+    public void negatiivinenOttoToimii() {
+         varasto.lisaaVarastoon(8);
+        double d = varasto.otaVarastosta(-2);
+        assertEquals(0.0, d, vertailuTarkkuus);
+    }
+
+    @Test
+    public void liikaOttoToimii() {
+        varasto.lisaaVarastoon(8);
+        double d = varasto.otaVarastosta(12);
+        assertEquals(8.0, d, vertailuTarkkuus);
+    }
+
+    @Test
+    public void LisataanliikaaTavaraa(){
+        varasto.lisaaVarastoon(20);
+        assertEquals(10.0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otetaanLiikaa() {
+        varasto.lisaaVarastoon(8);
+
+        double saatuMaara = varasto.otaVarastosta(9);
+
+        assertEquals(8.0,saatuMaara, vertailuTarkkuus );
+    }
+
+
+    @Test
+    public void alkusaldoToimiiNormaalisti(){
+        varasto = new Varasto(10,5);
+        assertEquals(5.0, varasto.getSaldo(), vertailuTarkkuus);
+
+    }
+
+    @Test
+    public void alkusaldoToimiiKunSaldoOnliikaa(){
+        varasto = new Varasto(10,15);
+        assertEquals(10.0, varasto.getSaldo(), vertailuTarkkuus);
+
+    }
+
+
+    @Test
+    public void alkusaldoToimiiKunTilavuusOnNegatiivinen() {
+        varasto = new Varasto(-10, 5);
+        assertEquals(0.0, varasto.getTilavuus(), vertailuTarkkuus);
+
+    }
+
+    @Test
+    public void alkusaldoToimiiKunSaldoOnNegatiivinen() {
+        varasto = new Varasto(10, -5);
+        assertEquals(0.0, varasto.getSaldo(), vertailuTarkkuus);
+
+    }
+
+
+
 
 }
